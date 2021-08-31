e3c1116d179f9bcf5ece34a2ca320b941ce36d03
CharayaWilkins <rayawilkins@gmail.com>
2021-08-31 16:52:41 -0700

    changes made

--- js/index.js
+++ js/index.js
@@ -30,5 +30,26 @@ const contentImg = document.querySelector('img-content');
 const  mapImg = contentImg.querySelector('img');
 
 //Mid Content
-const inverseContent = document.querySelector('.content-sectio inverse-content');
+const inverse = document.querySelector('.content-sectio inverse-content');
 const boatImg = document.querySelector('.img-fluid rounded');
+const inverseHead = inverse.querySelector('h2');
+const inverseCon1 = inverse.querySelector('p:nth-of-type(1)');
+const inverseCon2 = inverse.querySelector('p:nth-of-type(2)');
+
+//Destination (bottom) Content
+const destination = document.querySelector('.destination');
+const destination2 = destination.nextElementSibling;
+const destination3 = destination.nextElementSibling;
+const d1Heading = destination.querySelector('h4');
+const d1Content = destination.querySelector('p');
+const d1btn = destination.querySelector('.btn');
+const d2Heading = destination2.querySelector('h4');
+const d2Content = destination2.querySelector('p');
+const d2btn = destination2.querySelector('.btn');
+const d3Heading = destination3.querySelector('h4');
+const d3Content = destination3.querySelector('p');
+const d3btn = destination3.querySelector('.btn');
+
+//footer
+const footer = document.querySelector('.footer');
+const copywright = footer.querySelector('p');
